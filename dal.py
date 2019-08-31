0d32222cdd037b61449c72ae3a2f206aabdedaf6
Sohail Chamadia <sohailyc@gmail.com>
2019-09-01 04:03:37 +0530

    Changes in router and returning data

--- dal.py
+++ dal.py
@@ -1,15 +1,33 @@
 import pyodbc
 from sqlalchemy import create_engine
 import urllib
+import pandas as pd
 
-def insert_treatment(emp_id, treatment):
+def get_approval_list():
+    try:
+        conn_string = """Driver={ODBC Driver 17 for SQL Server};Server=skillenza.database.windows.net,1433;
+                        Database=skillenza;Uid=anonymous@skillenza;Pwd=test_user123;Encrypt=yes;TrustServerCertificate=no;
+                        Connection Timeout=30;"""
+        conn = pyodbc.connect(conn_string)
+        
+        df = pd.read_sql("SELECT * FROM treatment_details", conn)
+
+        conn.close()
+
+        return df
+
+    except Exception as e:
+        raise e
+
+
+def insert_treatment(emp_id, emp_name, treatment):
     try:
         conn_string = """Driver={ODBC Driver 17 for SQL Server};Server=skillenza.database.windows.net,1433;
                         Database=skillenza;Uid=anonymous@skillenza;Pwd=test_user123;Encrypt=yes;TrustServerCertificate=no;
                         Connection Timeout=30;"""
         conn = pyodbc.connect(conn_string)
         cursor = conn.cursor()
-        cursor.execute("INSERT INTO treatment_details VALUES (?,?,'pending')", emp_id, treatment)
+        cursor.execute("INSERT INTO treatment_details (emp_id, emp_name, treatment, approval_status) VALUES (?,?,?,'pending')", emp_id, emp_name, treatment)
 
         cursor.commit()
         cursor.close()
--- router.py
+++ router.py
@@ -8,7 +8,7 @@ import os
 
 from http import HTTPStatus
 
-from dal import insert_emp, insert_treatment
+from dal import insert_emp, insert_treatment, get_approval_list
 
 router = Blueprint('prediction', 'prediction')
 
@@ -23,6 +23,18 @@ def model_check():
         return Response(status=HTTPStatus.OK)
     except Exception as e:
         return Response(str(e),status=HTTPStatus.BAD_REQUEST)
+
+@router.route("/approval_list")
+def approvals():
+    try:
+
+        df = get_approval_list()
+
+        return Response(df)
+
+    except Exception as e:
+        return Response(str(e),status=HTTPStatus.BAD_REQUEST)
+
 
 @router.route("/predict",methods=['POST',])
 def predict():
@@ -45,7 +57,7 @@ def predict():
 
         prediction = encoders['treatment'].inverse_transform(classifier.predict(observation_df))
 
-        insert_treatment(df['emp_id'][0], prediction[0])
+        insert_treatment(df['emp_id'][0], df['emp_name'][0], prediction[0])
 
         return Response("Your response has been recorded and sent to your manager for approval.",status=HTTPStatus.OK)
 
